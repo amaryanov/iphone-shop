1fadd0d846ab6f3ec72209c038cc2ba3c8a4487a
amaryanov <amaryanov@d1f80348-5653-0410-97e1-55d09e47639f>
2008-08-04 20:40:16 +0000

    My implementation of Interface.

---
+++ anton/iShop/Classes/iShopAppDelegate.m
@@ -0,0 +1,45 @@
+//
+//  iShopAppDelegate.m
+//  iShop
+//
+//  Created by Anton Maryanov on 8/4/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import "iShopAppDelegate.h"
+
+
+@implementation iShopAppDelegate
+
+@synthesize window;
+@synthesize tabBarController;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+	
+	// Add the tab bar controller's current view as a subview of the window
+	[window addSubview:tabBarController.view];
+}
+
+
+/*
+ Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+}
+*/
+
+/*
+ Optional UITabBarControllerDelegate method
+- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed {
+}
+*/
+
+
+- (void)dealloc {
+	[tabBarController release];
+	[window release];
+	[super dealloc];
+}
+
+@end
+
