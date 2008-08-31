f553ca2953d60e34c2318406bec46ca0862d6be4
amaryanov <amaryanov@d1f80348-5653-0410-97e1-55d09e47639f>
2008-08-31 22:14:45 +0000

    Images gallery implemented

--- UI/iShop/Classes/iShopAppDelegate.m
+++ UI/iShop/Classes/iShopAppDelegate.m
@@ -14,13 +14,19 @@
 @synthesize window;
 @synthesize tabBarController;
 @synthesize navController;
+@synthesize tabBar;
 
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
 	
 	// Add the tab bar controller's current view as a subview of the window
 	[window addSubview:tabBarController.view];
+	mainDelegate = self;
 //	[window addSubview:navController.view];
+}
++(iShopAppDelegate*)MainDelegate
+{
+	return mainDelegate;
 }
 
 
