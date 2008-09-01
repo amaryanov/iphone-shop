c555a4f9d4ff80e8abe0fcc879e42ffd8fefce7b
konovalovandrei <konovalovandrei@d1f80348-5653-0410-97e1-55d09e47639f>
2008-09-01 23:34:18 +0000

    with "load more"

---
+++ trunk/UI/iShop/Classes/LoadMoreViewCell.m
@@ -0,0 +1,36 @@
+//
+//  LoadMoreViewCell.m
+//  iShop
+//
+//  Created by Andrey Konovalov on 9/2/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "LoadMoreViewCell.h"
+
+
+@implementation LoadMoreViewCell
+@synthesize label;
+
+- (id)initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier {
+	if (self = [super initWithFrame:frame reuseIdentifier:reuseIdentifier]) {
+		// Initialization code
+	}
+	return self;
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+
+	[super setSelected:selected animated:animated];
+
+	// Configure the view for the selected state
+}
+
+
+- (void)dealloc {
+	[super dealloc];
+}
+
+
+@end
