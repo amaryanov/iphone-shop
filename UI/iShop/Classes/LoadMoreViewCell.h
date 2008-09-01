077d84107c457f1be4b068ab65f429e4b9bba5d9
konovalovandrei <konovalovandrei@d1f80348-5653-0410-97e1-55d09e47639f>
2008-09-01 23:34:18 +0000

    with "load more"

---
+++ UI/iShop/Classes/LoadMoreViewCell.h
@@ -0,0 +1,17 @@
+//
+//  LoadMoreViewCell.h
+//  iShop
+//
+//  Created by Andrey Konovalov on 9/2/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoadMoreViewCell : UITableViewCell {
+	IBOutlet UILabel *label;
+}
+@property (nonatomic, retain) UILabel *label;
+
+@end
